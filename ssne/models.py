b29f09dfadaa9328fb7263a87e9c8177b9acde06
kaio <kaio.cruz@escolar.ifrn.edu.br>
2026-08-31 11:43:07 -0300

    models inicio

--- ssne/models.py
+++ ssne/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Evento(models.Model):
+    nome = models.CharField(max_length=100)
+    datare = models.DateField(auto_now=True)
+    dataev = models.DateField()
+    local = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
+
+class Aluno(models.Model):
+    class Curso(models.TextChoices):
+        info = 'INFO', 'Informática'
+        mamb = 'MAMB', 'Meio Ambiente'
+        edific = 'EDIFIC', 'Edificacoes'
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    senha = models.CharField(max_length=100)
+    curso = models.CharField(max_length=15, choices=Curso.choices)
+    ano = models.IntegerField
