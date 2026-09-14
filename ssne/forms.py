8477e4ed97227ee43b6a96354acdcd8c5e1e3a9e
JulioMigueLima <fernandeslimajulio@gmail.com>
2026-09-14 18:23:31 -0300

    Mexendo no form

--- ssne/forms.py
+++ ssne/forms.py
@@ -1,25 +1,25 @@
 from django import forms
-from .models import Mensagem, Post, User
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Row, Column, Submit
 from django.contrib.auth.forms import UserCreationForm as DjangoUserCreationForm
 from django.contrib.auth.forms import UsernameField
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Row, Column, Submit
+
+from .models import User, Curso, Setor, Aviso, Mensagem
+
 
 class UserCreationForm(DjangoUserCreationForm):
+    curso = forms.ChoiceField(choices=Curso.choices, required=True, label="Curso")
+
     class Meta:
         model = User
-        fields = ("username",)
+        fields = ("username", "curso")
         field_classes = {"username": UsernameField}
 
-class MensagemForm(forms.ModelForm):
-    class Meta:
-        model = Mensagem
-        fields = "__all__"
 
-class PostForm(forms.ModelForm):
+class SetorForm(forms.ModelForm):
 
     class Meta:
-        model = Post
+        model = Setor
         fields = "__all__"
 
     def __init__(self, *args, **kwargs):
@@ -28,8 +28,38 @@ class PostForm(forms.ModelForm):
         self.helper.form_method = 'post'
         self.helper.layout = Layout(
             Row(
-                Column('titulo', css_class='col-md-6'),
-                Column('autor', css_class='col-md-6'),
+                Column('nome', css_class='col-md-6'),
+                Column('bloco', css_class='col-md-6'),
+                css_class='row'
+            ),
+            Row(
+                Column('categoria', css_class='col-md-4'),
+                Column('posicao_x', css_class='col-md-4'),
+                Column('posicao_y', css_class='col-md-4'),
+                css_class='row'
+            ),
+            Row(
+                Column('descricao', css_class='col-12'),
+                css_class='row'
+            ),
+            Submit('submit', 'Salvar', css_class='btn btn-primary')
+        )
+
+
+class AvisoForm(forms.ModelForm):
+
+    class Meta:
+        model = Aviso
+        fields = ("titulo", "curso", "imagem", "conteudo")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            Row(
+                Column('titulo', css_class='col-md-8'),
+                Column('curso', css_class='col-md-4'),
                 css_class='row'
             ),
             Row(
@@ -37,8 +67,14 @@ class PostForm(forms.ModelForm):
                 css_class='row'
             ),
             Row(
-                Column('texto', css_class='col-12'),
+                Column('conteudo', css_class='col-12'),
                 css_class='row'
             ),
-            Submit('submit', 'Enviar', css_class='btn btn-primary')
+            Submit('submit', 'Publicar', css_class='btn btn-primary')
         )
+
+
+class MensagemForm(forms.ModelForm):
+    class Meta:
+        model = Mensagem
+        fields = ("nome", "email", "mensagem")
