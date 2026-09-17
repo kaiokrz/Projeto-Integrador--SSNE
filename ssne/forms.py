ee75fe680046c5a2462bfdf02a181172aad0aebe
luiz kaio <kaioluizcruzrezende@gmail.com>
2026-09-16 22:35:00 -0300

    aviso e setor:  forms views e html

--- ssne/forms.py
+++ ssne/forms.py
@@ -4,6 +4,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Row, Column, Submit
 from django.contrib.auth.forms import UserCreationForm as DjangoUserCreationForm
 from django.contrib.auth.forms import UsernameField
+from .models import User, Curso, Setor, Aviso, Mensagem
 
 class UserCreationForm(DjangoUserCreationForm):
     class Meta:
@@ -42,3 +43,56 @@ class PostForm(forms.ModelForm):
             ),
             Submit('submit', 'Enviar', css_class='btn btn-primary')
         )
+class SetorForm(forms.ModelForm):
+
+    class Meta:
+        model = Setor
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            Row(
+                Column('nome', css_class='col-md-6'),
+                Column('bloco', css_class='col-md-6'),
+                css_class='row'
+            ),
+            Row(
+                Column('categoria', css_class='col-md-4'),
+                Column('posicao_x', css_class='col-md-4'),
+                Column('posicao_y', css_class='col-md-4'),
+                css_class='row'
+            ),
+            Row(
+                Column('descricao', css_class='col-12'),
+                css_class='row'
+            ),
+            Submit('submit', 'Salvar', css_class='btn btn-primary'))
+
+class AvisoForm(forms.ModelForm):
+
+    class Meta:
+        model = Aviso
+        fields = ("titulo", "curso", "imagem", "conteudo")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            Row(
+                Column('titulo', css_class='col-md-8'),
+                Column('curso', css_class='col-md-4'),
+                css_class='row'
+            ),
+            Row(
+                Column('imagem', css_class='col-12'),
+                css_class='row'
+            ),
+            Row(
+                Column('conteudo', css_class='col-12'),
+                css_class='row'
+            ),
+            Submit('submit', 'Publicar', css_class='btn btn-primary'))
--- ssne/views.py
+++ ssne/views.py
@@ -1,4 +1,117 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required, permission_required
+from .models import Setor, Aviso
+from .forms import SetorForm, AvisoForm
 
 def index(request): 
     return render(request, "ssne/index.html")
+
+@login_required
+@permission_required("ssne.add_setor")
+def novo_setor(request):
+    if request.method == "POST":
+        form = SetorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Setor cadastrado com sucesso!')
+            return redirect("mapa")
+    else:
+        form = SetorForm()
+
+    context = {
+        "form": form,
+    }
+    return render(request, "ssne/form_setor.html", context)
+
+
+def setor_detalhe(request, id_setor):
+    context = {
+        "setor": get_object_or_404(Setor, id=id_setor),
+    }
+    return render(request, "ssne/setor_detalhe.html", context)
+
+@login_required
+@permission_required("ssne.change_setor")
+def editar_setor(request, id_setor):
+    setor = get_object_or_404(Setor, id=id_setor)
+    if request.method == "POST":
+        form = SetorForm(request.POST, instance=setor)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Setor editado com sucesso!')
+            return redirect("setor_detalhe", id_setor=setor.id)
+    else:
+        form = SetorForm(instance=setor)
+
+    context = {
+        "form": form,
+        "is_editar": True,
+    }
+    return render(request, "ssne/form_setor.html", context)
+
+
+@login_required
+@permission_required("ssne.delete_setor")
+def remover_setor(request, id_setor):
+    setor = get_object_or_404(Setor, id=id_setor)
+    if request.method == "POST":
+        setor.delete()
+        messages.success(request, 'Setor removido com sucesso!')
+        return redirect("mapa")
+    else:
+        context = {"titulo_objeto": setor.nome}
+        return render(request, "ssne/confirmar_remocao.html", context)
+
+
+@login_required
+@permission_required("ssne.add_aviso")
+def novo_aviso(request):
+    if request.method == "POST":
+        form = AvisoForm(request.POST, request.FILES)
+        if form.is_valid():
+            aviso = form.save(commit=False)
+            aviso.autor = request.user
+            aviso.save()
+            messages.success(request, 'Aviso publicado com sucesso!')
+            return redirect("index")
+    else:
+        form = AvisoForm()
+
+    context = {
+        "form": form,
+    }
+    return render(request, "ssne/form_aviso.html", context)
+
+
+@login_required
+@permission_required("ssne.change_aviso")
+def editar_aviso(request, id_aviso):
+    aviso = get_object_or_404(Aviso, id=id_aviso)
+    if request.method == "POST":
+        form = AvisoForm(request.POST, request.FILES, instance=aviso)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Aviso editado com sucesso!')
+            return redirect("index")
+    else:
+        form = AvisoForm(instance=aviso)
+
+    context = {
+        "form": form,
+        "is_editar": True,
+    }
+    return render(request, "ssne/form_aviso.html", context)
+
+
+@login_required
+@permission_required("ssne.delete_aviso")
+def remover_aviso(request, id_aviso):
+    aviso = get_object_or_404(Aviso, id=id_aviso)
+    if request.method == "POST":
+        aviso.delete()
+        messages.success(request, 'Aviso removido com sucesso!')
+        return redirect("index")
+    else:
+        context = {"titulo_objeto": aviso.titulo}
+        return render(request, "ssne/confirmar_remocao.html", context)
