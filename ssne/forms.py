931320a0dc9a5f016b6d44d9e896e9fe3806dd79
Kaio <kaio.cruz@escolar.ifrn.edu.br>
2026-09-17 11:55:11 -0300

    aviso e setor form so q agr no main

--- ssne/forms.py
+++ ssne/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Mensagem, Post, User
+from .models import Mensagem, User
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Row, Column, Submit
 from django.contrib.auth.forms import UserCreationForm as DjangoUserCreationForm
@@ -17,32 +17,6 @@ class MensagemForm(forms.ModelForm):
         model = Mensagem
         fields = "__all__"
 
-class PostForm(forms.ModelForm):
-
-    class Meta:
-        model = Post
-        fields = "__all__"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_method = 'post'
-        self.helper.layout = Layout(
-            Row(
-                Column('titulo', css_class='col-md-6'),
-                Column('autor', css_class='col-md-6'),
-                css_class='row'
-            ),
-            Row(
-                Column('imagem', css_class='col-12'),
-                css_class='row'
-            ),
-            Row(
-                Column('texto', css_class='col-12'),
-                css_class='row'
-            ),
-            Submit('submit', 'Enviar', css_class='btn btn-primary')
-        )
 class SetorForm(forms.ModelForm):
 
     class Meta:
